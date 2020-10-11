d0bb7ef243f9bbeb88c7ac8a8583a340d2729d26
João Victor Souza <jvms3d@gmail.com>
2020-10-11 10:31:17 -0300

    Updates

---
+++ precache-manifest.55ac1db7a8584cba411c5f2fbe27b6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249a9d94f09b9216d04e4875bb99cff5",
+    "url": "/monsterdex/index.html"
+  },
+  {
+    "revision": "ec3a240da04a60ff972c",
+    "url": "/monsterdex/static/css/main.8377e157.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsterdex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterdex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3a240da04a60ff972c",
+    "url": "/monsterdex/static/js/main.4af64780.chunk.js"
+  },
+  {
+    "revision": "b0fd63d05eaa51d7140a",
+    "url": "/monsterdex/static/js/runtime-main.327fc95d.js"
+  }
+]);
